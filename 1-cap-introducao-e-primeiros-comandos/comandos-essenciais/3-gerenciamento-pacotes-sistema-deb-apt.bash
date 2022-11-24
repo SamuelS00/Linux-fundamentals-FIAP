10cf11ac7a1049dfaad0524132e74785d923a0a0
Samuel <samuelsoutosilva14@gmail.com>
2022-11-24 18:49:51 -0300

    feat: add command apt example

---
+++ 1-cap-introducao-e-primeiros-comandos/comandos-essenciais/3-gerenciamento-pacotes-sistema-deb-apt.bash
@@ -0,0 +1,18 @@
+# responsável pelo gerenciamento de pacotes em um sistema do tipo .deb
+
+apt update
+
+# O comando apresentado na Linha de comando # apt update atualiza a
+# referência dos repositores, ou seja, ele baixa a lista de pacotes disponíveis para
+# download com suas versões
+
+apt upgrade
+
+# O comando na Linha de comando # apt upgrade exibe a lista de pacotes que
+# estão atualizados e confirma se desejamos realmente atualizá-los
+
+apt search trezor
+
+# O comando apt possui um parâmetro “search”, que permite buscar por
+# pacotes antes de instalá-los.
+
