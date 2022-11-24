a77e4931965bf7210147e252319baccdbd056678
Samuel <samuelsoutosilva14@gmail.com>
2022-11-24 18:46:14 -0300

    feat: add command su and whoami example

---
+++ 1-cap-introducao-e-primeiros-comandos/comandos-essenciais/2-trocar-usuário.bash
@@ -0,0 +1,10 @@
+su
+
+# Por razões de segurança, é recomendávelque o usuário sempre opere com seu próprio usuário, com permissões restritas.
+# O  comando  sué  o  empregado  para  a  mudança  de  usuário.  
+# Em  geral,  ele  é atribuído  como  sendo  o  comando  “para  se  tornar  root”, mas,  na  realidade,
+# ele permite  que  nos  tornemos  qualquer  usuário  do  sistema,
+
+whoami
+
+# descobrir qual o usuário atul do sistema
