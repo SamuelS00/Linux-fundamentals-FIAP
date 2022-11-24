71860fd623a8dac347a77035ad7a4b1c98ec0f7d
Samuel <samuelsoutosilva14@gmail.com>
2022-11-24 19:22:39 -0300

    feat: add useradd command example

---
+++ 1-cap-introducao-e-primeiros-comandos/gerenciamento-de-usuarios/2-adicionar-usuário-useradd.bash
@@ -0,0 +1,5 @@
+sudo userapp user1 -d /path/user1 -p senha
+
+# o comando useradd deve ser utilizado. Utilize os parâmetros “-d” para informar o diretório
+# pessoal deste usuário, e “-p” para informar a senha (embora você possa rodar o
+# comando $ sudo passwd [usuario] logo na sequência):
