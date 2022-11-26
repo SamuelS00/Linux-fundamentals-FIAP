469fdde81ff7a5ba702616c4dc86f2fe23cdd46a
Samuel <samuelsoutosilva14@gmail.com>
2022-11-26 18:57:33 -0300

    feat: add command tail example

---
+++ 1-cap-introducao-e-primeiros-comandos/manipulacao-de-arquivos-e-pastas/10-exibir-linhas-finais-de-um-arquivo-tail.bash
@@ -0,0 +1,5 @@
+tail -n 10
+
+# O comando tail faz o inverso do comando head, e a palavra significa em
+# inglês “cauda”: exibe as últimas linhas do arquivo desejado. O parâmetro “-n”
+# também pode ser usado aqui para definir o número de linhas finais que desejo exibir.
