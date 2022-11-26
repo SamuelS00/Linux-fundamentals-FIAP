5a502cad07040095d43953f78c3877348aab982f
Samuel <samuelsoutosilva14@gmail.com>
2022-11-26 18:58:11 -0300

    feat: add command unzip example

---
+++ 1-cap-introducao-e-primeiros-comandos/manipulacao-de-arquivos-e-pastas/14-descompactar-arquivos-zip-unzip.bash
@@ -0,0 +1,9 @@
+unzip -tv arquivoZipado.zip
+
+# Existirão ocasiões em que você precisará descompactar arquivos do tipo
+# “zip”, popularmente conhecido como “deszipar
+# O parâmetro “-v” permitirá listar cada arquivo e diretório que ele está
+# descompactando. Sem o parâmetro, o comando faz o trabalho e devolve o prompt
+# da maneira mais silenciosa possível.
+# O parâmetro “-t” é útil e permite testar o arquivo antes de descompactá-lo,
+# assim sendo:
