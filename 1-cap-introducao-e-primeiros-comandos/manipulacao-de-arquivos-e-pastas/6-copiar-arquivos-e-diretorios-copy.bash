d96e4b735f9ba955c718523bab171c16702c0d2a
Samuel <samuelsoutosilva14@gmail.com>
2022-11-26 18:56:57 -0300

    feat: add command cp example

---
+++ 1-cap-introducao-e-primeiros-comandos/manipulacao-de-arquivos-e-pastas/6-copiar-arquivos-e-diretorios-copy.bash
@@ -0,0 +1,5 @@
+cp Arquivo2.txt /tmp/NovoNome2.txt
+
+# O comando cp é uma abreviatura do verbo inglês to copy e permite copiar
+# arquivos e diretórios, solicitando sempre qual o arquivo copiado e, logo depois, para
+# onde ele será copiado
