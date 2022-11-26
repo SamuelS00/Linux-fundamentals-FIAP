a3f4ea2b71ab589e5678e0b8e28d7e3b92834756
Samuel <samuelsoutosilva14@gmail.com>
2022-11-26 18:56:10 -0300

    feat: add command cd example

---
+++ 1-cap-introducao-e-primeiros-comandos/manipulacao-de-arquivos-e-pastas/3-mudar-de-diretorio-cd.bash
@@ -0,0 +1,6 @@
+cd
+
+# O comando é um acrônimo de change directory e permite mudar o diretório
+# atual. O diretório pode ser mencionado tanto com seu caminho completo ou absoluto
+# (/home/hpoyatos) quanto o caminho relativo, que recebe este nome porque é
+# relativo ao diretório que estamos naquele momento.
