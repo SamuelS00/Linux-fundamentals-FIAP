008a4a29fe3b1f5a983175bdfaab69e1276f7137
Samuel <samuelsoutosilva14@gmail.com>
2022-11-26 18:58:20 -0300

    feat: add command tar example

---
+++ 1-cap-introducao-e-primeiros-comandos/manipulacao-de-arquivos-e-pastas/15-descomprimir-arquivos-tar.bash
@@ -0,0 +1,9 @@
+tar -czvf exemplo2.tgz *.txt
+
+# tar permite empacotar e desempacotar arquivos 
+# vários arquivos se tornam um só e vice-versa
+
+# conta com quatro parâmetros: “-c” indica que o arquivo de
+# pacote está sendo criado, enquanto “-z” aponta que desejo comprimir o arquivo de
+# destino com gzip; “-v” é o sempre útil verbose, enquanto “-f”, desta vez, não é
+# force, e, sim, qual o nome do arquivo que irei gerar,
