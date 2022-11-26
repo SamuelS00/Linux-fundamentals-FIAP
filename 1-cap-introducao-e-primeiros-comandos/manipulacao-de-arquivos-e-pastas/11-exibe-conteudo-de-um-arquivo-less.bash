cb6d200ad58232dec16e2d2e109e471c527ec2f2
Samuel <samuelsoutosilva14@gmail.com>
2022-11-26 18:57:41 -0300

    feat: add command less example

---
+++ 1-cap-introducao-e-primeiros-comandos/manipulacao-de-arquivos-e-pastas/11-exibe-conteudo-de-um-arquivo-less.bash
@@ -0,0 +1,7 @@
+sudo less /var/log/messages
+
+# O comando less pode ser muito útil para visualizar arquivos muito grandes,
+# pois, com ele, podemos navegar no conteúdo do arquivo com as setas para cima e
+# para baixo, pois a exibição com cat é de difícil navegação (para cat, use
+# [CTRL]+[PAGE UP] e [CTRL]+[PAGE DOWN]). Como o comando less trava o
+# terminal, digite “q” (de quit) para sair do comando.
