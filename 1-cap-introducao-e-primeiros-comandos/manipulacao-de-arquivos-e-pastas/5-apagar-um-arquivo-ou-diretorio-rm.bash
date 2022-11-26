a10bfb69ab5671bc89c899875f40add1b17fb964
Samuel <samuelsoutosilva14@gmail.com>
2022-11-26 18:56:42 -0300

    feat: add command rm example

---
+++ 1-cap-introducao-e-primeiros-comandos/manipulacao-de-arquivos-e-pastas/5-apagar-um-arquivo-ou-diretorio-rm.bash
@@ -0,0 +1,16 @@
+rm Arquivo1.txt
+
+# O comando rm é uma abreviação para o verbo inglês to remove e permitirá
+# que apaguemos um arquivo ou um diretório
+
+
+rm -rf /novapasta
+
+# -r” significa “recursivo”, ou seja, se ao apagar “Nova Pasta2” ele tiver uma
+# resistência, ele entra os subdiretórios e os apaga primeiro, e seus subsubdiretórios
+# caso existam, em um verdadeiro efeito cascata
+
+# Como apagar um diretório com
+# outros 30 dentro dele se torna uma tarefa muito chata, a maioria adiciona um “-f” de
+# force, ou seja, “força uma barra” e não me peça confirmação, e aí o comando fica
+# realmente perigoso
