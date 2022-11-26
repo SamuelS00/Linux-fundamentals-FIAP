bf479840c2dded8f09debaf8d2ed06d42cfb87ef
Samuel <samuelsoutosilva14@gmail.com>
2022-11-26 18:57:07 -0300

    feat: add command mv example

---
+++ 1-cap-introducao-e-primeiros-comandos/manipulacao-de-arquivos-e-pastas/7-movendo-ou-renomeando-arquivos-e-diretorios-mv.bash
@@ -0,0 +1,5 @@
+mv Arquivo2.txt Arquivo1.txt
+
+# O comando mv é uma abreviatura do verbo inglês to move e permite mover
+# arquivos e diretórios de um ponto a outro, solicitando sempre a origem e depois o
+# destino
