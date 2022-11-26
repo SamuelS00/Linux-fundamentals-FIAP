3fe2f12845fdbe1d1e8c0d72d3cfc4416f239a54
Samuel <samuelsoutosilva14@gmail.com>
2022-11-26 18:37:37 -0300

    feat: add command ls example

---
+++ 1-cap-introducao-e-primeiros-comandos/manipulacao-de-arquivos-e-pastas/1-listar-arquivos-e-diretorios-ls.bash
@@ -0,0 +1,10 @@
+ls
+
+# O comando ls permite exibir informações dos arquivos e subdiretórios de um
+# determinado diretório
+
+ls -la
+
+# O comando ls é geralmente executado com dois parâmetros muito úteis: “-l”,
+# que exibe uma lista longa ou detalhada de informações, e o “-a”, que vem de all, ou
+# seja, traga todos os arquivos e diretórios, mesmo aqueles que são ocultos
