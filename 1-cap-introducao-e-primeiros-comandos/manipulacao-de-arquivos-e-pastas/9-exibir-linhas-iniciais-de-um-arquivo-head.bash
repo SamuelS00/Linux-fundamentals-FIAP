8963411aecfbef417c89ca4db41470022c4823ac
Samuel <samuelsoutosilva14@gmail.com>
2022-11-26 18:57:25 -0300

    feat: add command head example

---
+++ 1-cap-introducao-e-primeiros-comandos/manipulacao-de-arquivos-e-pastas/9-exibir-linhas-iniciais-de-um-arquivo-head.bash
@@ -0,0 +1,10 @@
+head 
+
+head -n 10
+
+# o comando head permite exibir o cabeçalho do arquivo,
+# ou seja, é o comando cat que mostra apenas as linhas iniciais do arquivo
+
+# o parâmetro “-n”, que permite determinar o número
+# de linhas iniciais que desejo ver do arquivo e está sendo usado para exibir as dez
+# primeiras linhas do arquivo
