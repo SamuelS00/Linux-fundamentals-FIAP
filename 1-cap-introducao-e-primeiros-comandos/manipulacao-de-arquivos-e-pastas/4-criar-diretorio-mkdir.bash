b886da295e2a94037be172b6d4eef1de1ce90394
Samuel <samuelsoutosilva14@gmail.com>
2022-11-26 18:56:22 -0300

    feat: add command mkdir example

---
+++ 1-cap-introducao-e-primeiros-comandos/manipulacao-de-arquivos-e-pastas/4-criar-diretorio-mkdir.bash
@@ -0,0 +1,9 @@
+mkdir novapasta
+
+mkdir -p novapasta/subpasta/subsubpasta
+
+# O comando mkdir é uma abreviação da frase inglesa “make a directory”, que
+# significa literalmente “criar um diretório”
+
+# Um parâmetro muito útil para acompanhar o mkdir é o “-p”, especialmente em
+# situações que eu esteja criando subdiretórios,
