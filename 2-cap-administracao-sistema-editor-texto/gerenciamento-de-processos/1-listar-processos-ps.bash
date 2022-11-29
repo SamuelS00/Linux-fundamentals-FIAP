2029fa5ff533891951c924d6ad03901cee60858b
Samuel <samuelsoutosilva14@gmail.com>
2022-11-29 20:52:44 -0300

    feat: add command ps example

---
+++ 2-cap-administracao-sistema-editor-texto/gerenciamento-de-processos/1-listar-processos-ps.bash
@@ -0,0 +1,7 @@
+ps -au
+
+# O comando “ps” permite listar os processos atuais de um sistema e é,
+# geralmente, acompanhado pelos parâmetros: “-a”, que mostra os processos de
+# todos os usuários (permissão de root para tal), 
+#“-u”, que exibe uma lista detalhada
+# contendo o usuário dono do processo
