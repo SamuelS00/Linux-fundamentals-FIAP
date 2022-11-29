fe3b297abba0cf8892c928f3eae6f3681c6216cd
Samuel <samuelsoutosilva14@gmail.com>
2022-11-29 20:53:57 -0300

    feat: add command screen example

---
+++ 2-cap-administracao-sistema-editor-texto/gerenciamento-de-processos/5-criando-terminais-virtuais-screen.bash
@@ -0,0 +1,12 @@
+# processo sempre estarão associados a terminais.
+
+# há formas de criar terminais virtuais que podem ser abandonados e retomados em
+# um segundo momento.
+
+# sudo apt install screen, instalar o pacote
+
+screen -S terminal3
+
+# mostrar um terceiro terminal criado e abandonado com ctrl-1 e d.
+
+screen -ls
