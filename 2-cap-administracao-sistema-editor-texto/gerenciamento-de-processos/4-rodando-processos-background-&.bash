5f831f96c83b241a17a1b581e21d396353805621
Samuel <samuelsoutosilva14@gmail.com>
2022-11-29 20:53:44 -0300

    feat: add & example - rodando em background

---
+++ 2-cap-administracao-sistema-editor-texto/gerenciamento-de-processos/4-rodando-processos-background-&.bash
@@ -0,0 +1,8 @@
+comando + &
+
+# Todo comando que executa em um terminal, ao seu final, devolve o prompt
+# de comando para a execução de novos comandos, mas até o seu encerramento, ele
+# “tratava” o terminal, não permitindo novas interações do usuário
+
+# Isso acontece porque o processo está rodando em foreground ou primeiro
+# plano.
