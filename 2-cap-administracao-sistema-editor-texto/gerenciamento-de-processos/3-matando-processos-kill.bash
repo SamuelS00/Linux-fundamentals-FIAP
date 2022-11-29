13ab8f4dd851fe884286bb04657c32351ba2b6bc
Samuel <samuelsoutosilva14@gmail.com>
2022-11-29 20:53:02 -0300

    feat: add command kill example

---
+++ 2-cap-administracao-sistema-editor-texto/gerenciamento-de-processos/3-matando-processos-kill.bash
@@ -0,0 +1,5 @@
+kill pid
+
+# um administrador de sistema precisa interromper processos
+# em andamento de maneira abrupta, seja por estarem consumindo recursos
+# indesejados, seja por representarem algum risco ao sistema.
