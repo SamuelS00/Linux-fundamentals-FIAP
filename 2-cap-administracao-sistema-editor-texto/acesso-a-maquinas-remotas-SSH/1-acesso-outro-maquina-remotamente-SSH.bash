db2f0e066fd66dec995980b338d935085ebeb879
Samuel <samuelsoutosilva14@gmail.com>
2022-11-29 20:52:11 -0300

    feat: add command ssh example

---
+++ 2-cap-administracao-sistema-editor-texto/acesso-a-maquinas-remotas-SSH/1-acesso-outro-maquina-remotamente-SSH.bash
@@ -0,0 +1,14 @@
+# O SSH (Secure Shell ou shell seguro) é a evolução do telnet, permitindo toda
+# a praticidade de um acesso remoto com uma diferença: um túnel criptografado,
+# garantindo a privacidade e evitando ataques.
+# substituindo o telnet
+
+# OpenSSH, configurador de um serviço SSH.
+
+# SSH é o cliente para acesso a terminais remotos.
+#   @user ( nome do usuario ) 
+#   o nome o ip do host
+#   porta padrão 22, se não tiver na porta padrão usar o param -p 
+
+
+ssh @user 000.000.0.000
