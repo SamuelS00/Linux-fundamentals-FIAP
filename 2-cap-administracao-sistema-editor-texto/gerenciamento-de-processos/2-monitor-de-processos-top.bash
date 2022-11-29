8fa8936d014fbbf88cea405b51cefefd8be947bd
Samuel <samuelsoutosilva14@gmail.com>
2022-11-29 20:52:53 -0300

    feat: add command top example

---
+++ 2-cap-administracao-sistema-editor-texto/gerenciamento-de-processos/2-monitor-de-processos-top.bash
@@ -0,0 +1,6 @@
+top
+
+# O comando top não é apenas um monitor de processos, ele permite obter
+# informações importantes, como o total de processos em andamento, seus estados
+# no momento (executando, em espera, parados e zombies), uso de CPU, memória
+# RAM e memória de swap.
