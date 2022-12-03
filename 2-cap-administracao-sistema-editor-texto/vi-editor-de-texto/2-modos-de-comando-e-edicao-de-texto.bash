4527575b5adb5d55a2b036960857a14677837db6
Samuel <samuelsoutosilva14@gmail.com>
2022-12-03 18:40:50 -0300

    feat: add commands vi example

---
+++ 2-cap-administracao-sistema-editor-texto/vi-editor-de-texto/2-modos-de-comando-e-edicao-de-texto.bash
@@ -0,0 +1,9 @@
+# a tecla [i] entrar em modo de edição de texto no ponto em que o cursor estiver no momento.
+
+# [ESC] volta ao modo de comandos
+
+# a tecla [a] entrar em modo edição após o ponto em que o cursor estiver no momento
+
+# a tecla [A] entrar em modo de edição no final da frase em que estamos.
+
+# a tecla [o] entrar em modo de edição na linha seguinte àquela em que estamos.
