a7a9131a3fee3214b82429e1e60448e5885f2aa6
Samuel <samuelsoutosilva14@gmail.com>
2022-12-03 18:43:29 -0300

    feat: add command to erase line vi example

---
+++ 2-cap-administracao-sistema-editor-texto/vi-editor-de-texto/6-apagando-texto.bash
@@ -0,0 +1,11 @@
+# digitar dd em modo comando apaga uma única linha
+
+# [dw] apaga a palavra do ponto atual
+
+# [d$] apaga do ponto atual até o final da linha
+
+# [d^] apaga do ponto autal até o inicio
+
+# [u] + enter -> desfazer o ÚLTIMO comando
+
+# ao usar o comando [d] está sendo feito um recorte utilizando o p posso colar o conteúdo
