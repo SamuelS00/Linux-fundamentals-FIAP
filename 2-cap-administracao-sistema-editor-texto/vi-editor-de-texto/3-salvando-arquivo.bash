db47cd5212a56074409cd72f15e155c492db0057
Samuel <samuelsoutosilva14@gmail.com>
2022-12-03 18:41:32 -0300

    feat: add commands to save files vi example

---
+++ 2-cap-administracao-sistema-editor-texto/vi-editor-de-texto/3-salvando-arquivo.bash
@@ -0,0 +1,7 @@
+# para salvar o arquivo devo estar em modo de comandos.
+
+# utilizar [:], [w] (de write) e [ENTER] -> para salvar arquivo
+
+# utilizar w e q salvar e sair arquivo
+
+# [:] [x] [enter] tbm funciona
